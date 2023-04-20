05cf0d938aa98f253dde3351d76776d57d543d06
IonStroons <Philiwi1995@googlemail.com>
2023-04-20 15:32:31 +0200

    Correction File System

--- public/script.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-//setting middleware
-app.use(express.static(__dirname + 'public')); //Serves resources from public folder
-
-
-var server = app.listen(3000);
